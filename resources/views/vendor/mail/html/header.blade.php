1d5cf088aef59455d977e029eeb2067dc64167de
gabj1-gildo <gabj1@aluno.ifnmg.edu.br>
2026-02-06 21:48:46 -0300

    atualização visual nos emails

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,12 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="https://piravolei.online/img/logo.png" class="logo" alt="PiraVôlei">
+@else
+{!! $slot !!}
+@endif
+</a>
+</td>
+</tr>
